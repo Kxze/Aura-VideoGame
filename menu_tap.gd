cfef72539b35ebeb4217a669175b561b8ab5b722
gillianarce <gillian.arce4761@alumnos.udg.mx>
2025-09-16 22:04:01 -0600

    menu tap listo

---
+++ menu_tap.gd
@@ -0,0 +1,13 @@
+extends Control
+
+func _unhandled_input(event):
+	# Si se presiona cualquier tecla
+	if event is InputEventKey and event.pressed:
+		_cambiar_a_menu()
+
+	# Si se hace clic con el mouse
+	if event is InputEventMouseButton and event.pressed:
+		_cambiar_a_menu()
+
+func _cambiar_a_menu():
+	get_tree().change_scene_to_file("res://scenes/menu_principal.tscn")
