4f7b8881ee582969e6e60fa90dc2092acc8f4483
gillianarce <gillian.arce4761@alumnos.udg.mx>
2025-09-25 17:54:10 -0600

    estrellas y glow arreglado

--- scenes/v_box_container.gd
+++ scenes/v_box_container.gd
@@ -25,7 +25,6 @@ func _ready():
 
 		# Al inicio ocultamos estrellas y glow
 		_hide_stars(b)
-		_enable_glow(b, false)
 
 	# Activar el primer botón al inicio
 	_set_active_button(0)
@@ -69,12 +68,10 @@ func _set_active_button(index: int):
 	# Apagar todos
 	for b in _buttons:
 		_hide_stars(b)
-		_enable_glow(b, false)
 
 	# Encender solo el seleccionado
 	var selected = _buttons[index]
 	_show_stars(selected)
-	_enable_glow(selected, true)
 	_current_index = index
 	selected.grab_focus()
 
@@ -91,7 +88,3 @@ func _hide_stars(button):
 		button.get_node("HBoxContainer/StarLeft").visible = false
 	if button.has_node("HBoxContainer/StarRight"):
 		button.get_node("HBoxContainer/StarRight").visible = false
-
-func _enable_glow(button, enabled: bool):
-	if button.material and button.material is ShaderMaterial:
-		button.material.set_shader_parameter("active", enabled)
