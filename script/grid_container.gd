4916c445e18e88af8dc026573ce130f52c999f3e
mayopeel <marialepe8@gmail.com>
2025-09-30 18:22:18 -0600

    modificaciones de IU y controles
    Se agregó una pantalla para poder modificar los controles desde dentro del juego

--- script/grid_container.gd
+++ script/grid_container.gd
@@ -3,11 +3,22 @@ extends GridContainer
 @onready var check_btn: CheckButton = $CheckModo
 @onready var option_res: OptionButton = $OptionRES
 
+@onready var btn_controles: Button = $BtnControles
+
+@onready var margin_container: MarginContainer = $".."
+#@onready var grid_controles: GridContainer = $"../../GridContainerControles"
+
+
 var prev_selected: int = -1  # Guardará la opción previa
 
 func _ready() -> void:
+	#para desactivar el botón de resolución cuando es modo fullscreen
 	check_btn.toggled.connect(_on_check_btn_toggled)
 	option_res.item_selected.connect(_on_option_res_item_selected)
+	
+	btn_controles.pressed.connect(_on_btn_controles_pressed)
+	
+	#grid_controles.visible = false #GridContainerControles oculto al iniciar
 
 func _on_check_btn_toggled(pressed: bool) -> void:
 	if pressed:
@@ -22,6 +33,7 @@ func _on_check_btn_toggled(pressed: bool) -> void:
 			# Simula que el usuario eligió la opción
 			option_res.emit_signal("item_selected", prev_selected)
 
+#para cambiar la resolución según el # de la lista
 func _on_option_res_item_selected(index: int) -> void:
 	match index:
 		2:
@@ -30,3 +42,7 @@ func _on_option_res_item_selected(index: int) -> void:
 			DisplayServer.window_set_size(Vector2i(1600, 900))
 		0:
 			DisplayServer.window_set_size(Vector2i(1152, 648))
+
+func _on_btn_controles_pressed() -> void:
+	margin_container.visible = false   #Oculta el MarginContainer
+#	grid_controles.visible = true  # Muestra el GridContainerControles
---
+++ script/grid_container_controles.gd
@@ -0,0 +1,5 @@
+extends GridContainer
+
+var is_remapping = false
+var action_to_remap = null
+var remapping_button = null
