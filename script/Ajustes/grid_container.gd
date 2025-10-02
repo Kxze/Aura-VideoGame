1561bc4f5c37c9ede7ab11c6ecd3c0c27babe8bc
mayopeel <marialepe8@gmail.com>
2025-10-02 13:00:00 -0600

    cambios ajustes
    Nuevos botones, calidad, volumen, adición de audios de assets e intento de AudioManager

--- script/Ajustes/grid_container.gd
+++ script/Ajustes/grid_container.gd
@@ -1,15 +1,21 @@
 extends GridContainer
 
+#para el modo de pantalla y resolución
 @onready var check_btn: CheckButton = $CheckModo
 @onready var option_res: OptionButton = $OptionRES
 
+#para el cambio de entre ajustes y controles
 @onready var btn_controles: Button = $BtnControles
-
 @onready var margin_container: MarginContainer = $".."
 #@onready var grid_controles: GridContainer = $"../../GridContainerControles"
 
-
+#para el modo de pantalla y resolución
 var prev_selected: int = -1  # Guardará la opción previa
+
+#para los buses de audio
+var master_bus = AudioServer.get_bus_index("Master")
+var musica_bus = AudioServer.get_bus_index("Musica")
+var efectos_bus = AudioServer.get_bus_index("Efectos")
 
 func _ready() -> void:
 	#para desactivar el botón de resolución cuando es modo fullscreen
@@ -46,3 +52,24 @@ func _on_option_res_item_selected(index: int) -> void:
 func _on_btn_controles_pressed() -> void:
 	margin_container.visible = false   #Oculta el MarginContainer
 #	grid_controles.visible = true  # Muestra el GridContainerControles
+
+func _on_slider_vg_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(master_bus, value)
+	if value == -30:
+		AudioServer.set_bus_mute(master_bus, true)
+	else:
+		AudioServer.set_bus_mute(master_bus, false)
+
+func _on_slider_m_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(musica_bus, value)
+	if value == -30:
+		AudioServer.set_bus_mute(musica_bus, true)
+	else:
+		AudioServer.set_bus_mute(musica_bus, false)
+
+func _on_slider_sfx_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(efectos_bus, value)
+	if value == -30:
+		AudioServer.set_bus_mute(efectos_bus, true)
+	else:
+		AudioServer.set_bus_mute(efectos_bus, false)
