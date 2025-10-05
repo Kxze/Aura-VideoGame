332cf2210a71fdb11c1009a0e8c078bd13fa09af
mayopeel <marialepe8@gmail.com>
2025-10-04 20:00:36 -0600

    Pausa
    Se añadió la función de pausa, el popup de ajustes se aplica de manera global y cambia de viñetas dependiendo de si está abierto en el menú o si está abierto en el gameplay a modo de pausa (yaaay!!!)

--- script/Ajustes/Ajustes.gd
+++ script/Ajustes/Ajustes.gd
@@ -6,6 +6,14 @@ extends Popup
 @onready var btn_inicio: Button = $Panel/BtnInicio
 @onready var btn_salir: Button = $Panel/BtnSalir
 
+func _ready() -> void:
+	process_mode = Node.PROCESS_MODE_ALWAYS
+	# Conectamos las señales de los botones
+	btn_salir.pressed.connect(_on_btn_salir_pressed)
+	btn_inicio.pressed.connect(_on_btn_inicio_pressed)
+	btn_continuar.pressed.connect(_on_btn_continuar_pressed)
+	btn_cerrar.pressed.connect(_on_btn_cerrar_pressed)
+
 # Mostrar los botones del popup dependiendo del contexto
 func mostrar(origen: String):
 	match origen:
@@ -20,16 +28,20 @@ func mostrar(origen: String):
 			btn_inicio.visible = true
 			btn_salir.visible = true
 
-#para cerrar la ventana del pop up (aún no logro entender pq no sirve
+#para cerrar la ventana del pop up
 func _on_btn_cerrar_pressed() -> void:
 	popup_ajustes.visible = false
 	get_tree().change_scene_to_file("res://scenes/menu_principal.tscn")
 
 func _on_btn_continuar_pressed() -> void:
-	UiGlobal.popup_ajustes.hide()
-	get_tree().paused = false
+	get_tree().paused = false #por si estaba pausado
+	if has_node("/root/UiGlobal"):
+		get_node("/root/UiGlobal").cerrar_ajustes()
+	else:
+		hide()
 
 func _on_btn_inicio_pressed() -> void:
+	get_tree().paused = false  #por si estaba pausado
 	popup_ajustes.visible = false
 	get_tree().change_scene_to_file("res://scenes/menu_principal.tscn")
 
