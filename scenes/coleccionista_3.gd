6b504f03c2c10f6aa396ab1a2a1320d3f51fd61a
gillianarce <gillian.arce4761@alumnos.udg.mx>
2025-10-05 23:27:49 -0600

    sonido paginas coleccionista

--- scenes/coleccionista_3.gd
+++ scenes/coleccionista_3.gd
@@ -1,6 +1,7 @@
 extends Control
 
-@onready var click_sound = preload("res://sonidos/botón2.wav")   # 🔊 sonido click
+@onready var click_sound = preload("res://sonidos/botón2.wav")
+@onready var sonido_cambio_pagina = preload("res://sonidos/cambioPagina.wav")
 
 func _ready():
 	# Conectar botones
@@ -8,24 +9,34 @@ func _ready():
 	$salir.pressed.connect(_on_salir_pressed)
 
 func _input(event):
-	# Navegar con teclado
-	if event.is_action_pressed("ui_left"):  # 👈 Flecha Izquierda
+	# 👈 Navegar con Flecha Izquierda
+	if event.is_action_pressed("ui_left"):
 		_play_click()
-		get_tree().change_scene_to_file("res://scenes/coleccionista2.tscn")
+		_play_cambio_pagina_y_cambiar("res://scenes/coleccionista2.tscn")
 
-	elif event.is_action_pressed("ui_cancel"):  # ⎋ tecla Esc
+	# ⎋ Regresar con tecla Esc
+	elif event.is_action_pressed("ui_cancel"):
 		_play_click()
 		get_tree().change_scene_to_file("res://scenes/menu_principal.tscn")
 
-# --- Funciones de botones ---
+# --- Botones ---
 func _on_flecha_izq_pressed():
 	_play_click()
-	get_tree().change_scene_to_file("res://scenes/coleccionista2.tscn")
+	_play_cambio_pagina_y_cambiar("res://scenes/coleccionista2.tscn")
 
 func _on_salir_pressed():
 	_play_click()
 	get_tree().change_scene_to_file("res://scenes/menu_principal.tscn")
 
-# --- 🔊 Reproducir sonido ---
+# --- 🔊 Sonidos ---
 func _play_click():
-	AudioManager.play_click(click_sound)
+	if AudioManager:
+		AudioManager.play_click(click_sound)
+
+func _play_cambio_pagina_y_cambiar(scene_path: String):
+	if AudioManager:
+		# 🔊 Suena el cambio de página de manera persistente
+		AudioManager.play_sfx_persistente(sonido_cambio_pagina)
+
+	# Cambia de escena al instante (el sonido no se corta)
+	get_tree().change_scene_to_file(scene_path)
