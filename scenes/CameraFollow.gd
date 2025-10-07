fcfb5eb4a144ecb487518d4953565ea80f5551cb
gillianarce <gillian.arce4761@alumnos.udg.mx>
2025-10-07 13:44:20 -0600

    brillo arreglado

--- scenes/CameraFollow.gd
+++ scenes/CameraFollow.gd
@@ -8,7 +8,8 @@ extends Camera3D
 
 func _ready():
 	var env = $".".environment
-	GlobalBrightness.registrar_environment(env)
+	GlobalBrightnessWorldEnvironment.registrar_environment(env)
+
 
 func _physics_process(delta: float) -> void:
 	if not target:
--- script/Ajustes/slider_br.gd
+++ script/Ajustes/slider_br.gd
@@ -2,4 +2,4 @@ extends HSlider
 
 #para cambiar el brillo global del juego
 func _on_value_changed(value: float) -> void:
-	GlobalBrightness.set_brightness(value)
+	GlobalBrightnessWorldEnvironment.set_brightness(value)
